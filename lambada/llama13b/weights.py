eff9acfa50ea6af556e68d1469438925a60a2d19
Kanav Gupta <kanav0610@gmail.com>
2024-06-03 14:36:45 +0530

    add llama13b scripts

--- lambada/llama13b/weights.py
+++ lambada/llama13b/weights.py
@@ -5,9 +5,7 @@ from tqdm import tqdm
 import sys
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
-# model_path = 'openlm-research/open_llama_7b'
-# model_path = 'meta-llama/Llama-2-13b-chat-hf'
-model_path = 'meta-llama/Llama-2-7b-chat-hf'
+model_path = 'meta-llama/Llama-2-13b-chat-hf'
 
 model = AutoModelForCausalLM.from_pretrained(
     model_path)
@@ -17,8 +15,6 @@ for k in sd.keys():
     sd[k] = sd[k].cpu()
 
 f = open('weights.dat', 'wb')
-# f = open('meta_llama2_13b.dat', 'wb')
-# f = open(sys.argv[1], 'wb')
 
 def dumpvec(x):
     # print(x.shape)
@@ -30,13 +26,9 @@ def dumpmat(w):
     assert(len(w.shape) == 2)
     f.write(w.numpy().tobytes())
 
-for i in tqdm(range(32)):
-# for i in tqdm(range(40)):
+for i in tqdm(range(40)):
 
     dumpvec(sd['model.layers.%d.input_layernorm.weight' % i])
-
-    # c_attn_w = np.concatenate([sd['transformer.h.%d.attn.attention.q_proj.weight' % i].T, sd['transformer.h.%d.attn.attention.k_proj.weight' % i].T, sd['transformer.h.%d.attn.attention.v_proj.weight' % i].T], axis=-1)
-    # dumpmat(c_attn_w)
 
     dumpmat(sd['model.layers.%d.self_attn.q_proj.weight' % i].T)
     dumpmat(sd['model.layers.%d.self_attn.k_proj.weight' % i].T)
