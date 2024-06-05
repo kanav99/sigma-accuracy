ecb7143e307b3a268fcd88456b9ce9c3c679c282
Kanav Gupta <kanav0610@gmail.com>
2024-06-05 11:46:22 +0530

    mrpc large val

--- mrpc/large/validation.py
+++ mrpc/large/validation.py
@@ -3,12 +3,9 @@ from torch import nn
 import numpy as np
 from torch.optim import Adam
 from tqdm import tqdm
-from transformers import BertTokenizer, BertModel
-from datasets import load_dataset
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
-
+from datasets import load_dataset
 tokenizer = AutoTokenizer.from_pretrained('yoshitomo-matsubara/bert-large-uncased-mrpc')
-model = AutoModelForSequenceClassification.from_pretrained("yoshitomo-matsubara/bert-large-uncased-mrpc")
 
 sst2_val = load_dataset('SetFit/mrpc', split='validation')
 
@@ -18,7 +15,7 @@ class Dataset(torch.utils.data.Dataset):
 
         self.labels = [d['label'] for d in df]
         self.texts = [
-            tokenizer(d['text1'],d['text2'],
+            tokenizer(d['text1'], d['text2'],
                 padding='max_length', 
                 max_length = 512, 
                 truncation=True,
@@ -47,11 +44,32 @@ class Dataset(torch.utils.data.Dataset):
 
         return batch_texts, batch_y
 
+class BertClassifier(nn.Module):
+
+    def __init__(self, num_classes, dropout=0.5):
+
+        super(BertClassifier, self).__init__()
+
+        self.bert = AutoModelForSequenceClassification.from_pretrained('yoshitomo-matsubara/bert-large-uncased-mrpc').bert
+        self.dropout = nn.Dropout(dropout)
+        self.linear = AutoModelForSequenceClassification.from_pretrained('yoshitomo-matsubara/bert-large-uncased-mrpc').classifier
+        self.relu = nn.ReLU()
+
+    def forward(self, input_id, mask, token_type_ids):
+
+        _, pooled_output = self.bert(input_ids=input_id, attention_mask=mask, token_type_ids=token_type_ids, return_dict=False)
+        dropout_output = self.dropout(pooled_output)
+        linear_output = self.linear(dropout_output)
+        final_layer = self.relu(linear_output)
+
+        return final_layer
+
+
+model = BertClassifier(2)
 model.eval()
 
 total_acc_val = 0
-val_dataloader = torch.utils.data.DataLoader(Dataset(sst2_val), batch_size=8)
-# val_dataloader = torch.utils.data.DataLoader(Dataset(sst2_val), batch_size=32)
+val_dataloader = torch.utils.data.DataLoader(Dataset(sst2_val), batch_size=32)
 # for x in val_dataloader:
 #     print(x)
 
@@ -62,15 +80,14 @@ model = model.to(device)
 tot = 0
 with torch.no_grad():
 
-    for val_input, val_label in val_dataloader:
+    for val_input, val_label in tqdm(val_dataloader):
         tot += len(val_label)
         val_label = val_label.to(device)
         mask = val_input['attention_mask'].squeeze(1).to(device)
-        # token_type_ids = val_input['token_type_ids'].squeeze(1).to(device)
         input_id = val_input['input_ids'].squeeze(1).to(device)
+        token_type_ids = val_input['token_type_ids'].squeeze(1).to(device)
 
-        # output = model(input_id, mask, token_type_ids)
-        output = model(input_id, mask)
+        output = model(input_id, mask, token_type_ids)
         
         acc = (output.argmax(dim=1) == val_label).sum().item()
         total_acc_val += acc        
