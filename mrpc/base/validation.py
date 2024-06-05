a12f2fcfe61874b3fee2d4b6897313801a16cd82
Kanav Gupta <kanav0610@gmail.com>
2024-06-05 12:18:28 +0530

    base mrpc

---
+++ mrpc/base/validation.py
@@ -0,0 +1,95 @@
+import torch
+from torch import nn
+import numpy as np
+from torch.optim import Adam
+from tqdm import tqdm
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+from datasets import load_dataset
+tokenizer = AutoTokenizer.from_pretrained('yoshitomo-matsubara/bert-base-uncased-mrpc')
+
+sst2_val = load_dataset('SetFit/mrpc', split='validation')
+
+class Dataset(torch.utils.data.Dataset):
+
+    def __init__(self, df):
+
+        self.labels = [d['label'] for d in df]
+        self.texts = [
+            tokenizer(d['text1'], d['text2'],
+                padding='max_length', 
+                max_length = 512, 
+                truncation=True,
+                return_tensors="pt")
+            for d in df
+        ]
+
+    def classes(self):
+        return self.labels
+
+    def __len__(self):
+        return len(self.labels)
+
+    def get_batch_labels(self, idx):
+        # Fetch a batch of labels
+        return np.array(self.labels[idx])
+
+    def get_batch_texts(self, idx):
+        # Fetch a batch of inputs
+        return self.texts[idx]
+
+    def __getitem__(self, idx):
+
+        batch_texts = self.get_batch_texts(idx)
+        batch_y = self.get_batch_labels(idx)
+
+        return batch_texts, batch_y
+
+class BertClassifier(nn.Module):
+
+    def __init__(self, num_classes, dropout=0.5):
+
+        super(BertClassifier, self).__init__()
+
+        self.bert = AutoModelForSequenceClassification.from_pretrained('yoshitomo-matsubara/bert-base-uncased-mrpc').bert
+        self.dropout = nn.Dropout(dropout)
+        self.linear = AutoModelForSequenceClassification.from_pretrained('yoshitomo-matsubara/bert-base-uncased-mrpc').classifier
+        self.relu = nn.ReLU()
+
+    def forward(self, input_id, mask, token_type_ids):
+
+        _, pooled_output = self.bert(input_ids=input_id, attention_mask=mask, token_type_ids=token_type_ids, return_dict=False)
+        dropout_output = self.dropout(pooled_output)
+        linear_output = self.linear(dropout_output)
+        final_layer = self.relu(linear_output)
+
+        return final_layer
+
+
+model = BertClassifier(2)
+model.eval()
+
+total_acc_val = 0
+val_dataloader = torch.utils.data.DataLoader(Dataset(sst2_val), batch_size=32)
+# for x in val_dataloader:
+#     print(x)
+
+use_cuda = torch.cuda.is_available()
+device = torch.device("cuda" if use_cuda else "cpu")
+model = model.to(device)
+
+tot = 0
+with torch.no_grad():
+
+    for val_input, val_label in tqdm(val_dataloader):
+        tot += len(val_label)
+        val_label = val_label.to(device)
+        mask = val_input['attention_mask'].squeeze(1).to(device)
+        input_id = val_input['input_ids'].squeeze(1).to(device)
+        token_type_ids = val_input['token_type_ids'].squeeze(1).to(device)
+
+        output = model(input_id, mask, token_type_ids)
+        
+        acc = (output.argmax(dim=1) == val_label).sum().item()
+        total_acc_val += acc        
+
+print(total_acc_val, "/", tot, "=", 100*total_acc_val/tot, "%")
---
+++ mrpc/base/weights.py
@@ -0,0 +1,82 @@
+import torch
+import struct
+import numpy as np
+from tqdm import tqdm
+import sys
+from transformers import AutoModelForSequenceClassification
+
+# sd = torch.load(sys.argv[1], map_location=torch.device('cpu'))
+model = AutoModelForSequenceClassification.from_pretrained("yoshitomo-matsubara/bert-base-uncased-mrpc")
+sd = model.state_dict()
+
+for k in sd.keys():
+    sd[k] = sd[k].cpu()
+
+# f = open('bert_sst2_90_t.dat', 'wb')
+# f = open(sys.argv[2], 'wb')
+f = open("weights.dat", 'wb')
+
+def dumpvec(x):
+    # print(x.shape)
+    assert(len(x.shape) == 1)
+    CI = x.shape[0]
+    try:
+        x = x.numpy()
+    except:
+        pass
+    f.write(x.tobytes())
+
+def dumpmat(w):
+    # print(w.shape)
+    assert(len(w.shape) == 2)
+    CI = w.shape[0]
+    CO = w.shape[1]
+    try:
+        w = w.numpy()
+    except:
+        pass
+    f.write(w.tobytes())
+
+dumpvec(sd["bert.embeddings.LayerNorm.weight"])
+dumpvec(sd["bert.embeddings.LayerNorm.bias"])
+
+for i in tqdm(range(12)):
+    pref = "bert.encoder.layer." + str(i)
+    c_attn_w = np.concatenate([sd[pref + ".attention.self.query.weight"].T, sd[pref + ".attention.self.key.weight"].T, sd[pref + ".attention.self.value.weight"].T], axis=-1)
+    c_attn_b = np.concatenate([sd[pref + ".attention.self.query.bias"], sd[pref + ".attention.self.key.bias"], sd[pref + ".attention.self.value.bias"]], axis=-1)
+    # print(c_attn_w.shape)
+    dumpmat(c_attn_w)
+    dumpvec(c_attn_b)
+    c_proj_w = sd[pref + ".attention.output.dense.weight"].T
+    c_proj_b = sd[pref + ".attention.output.dense.bias"]
+    # print(c_proj_w.shape)
+    dumpmat(c_proj_w)
+    dumpvec(c_proj_b)
+    ln_0_w = sd[pref + ".attention.output.LayerNorm.weight"]
+    ln_0_b = sd[pref + ".attention.output.LayerNorm.bias"]
+    dumpvec(ln_0_w)
+    dumpvec(ln_0_b)
+    mlp_w = sd[pref + ".intermediate.dense.weight"].T
+    mlp_b = sd[pref + ".intermediate.dense.bias"]
+    dumpmat(mlp_w)
+    dumpvec(mlp_b)
+    # print(mlp_w.shape)
+    
+    mlp_w = sd[pref + ".output.dense.weight"].T
+    mlp_b = sd[pref + ".output.dense.bias"]
+    # print(mlp_w.shape)
+    dumpmat(mlp_w)
+    dumpvec(mlp_b)
+    ln_1_w = sd[pref + ".output.LayerNorm.weight"]
+    ln_1_b = sd[pref + ".output.LayerNorm.bias"]
+    dumpvec(ln_1_w)
+    dumpvec(ln_1_b)
+    
+dumpmat(sd["bert.pooler.dense.weight"].T)
+dumpvec(sd["bert.pooler.dense.bias"])
+# dumpmat(sd["linear.weight"].T)
+# dumpvec(sd["linear.bias"])
+dumpmat(sd["classifier.weight"].T)
+dumpvec(sd["classifier.bias"])
+
+f.close()
